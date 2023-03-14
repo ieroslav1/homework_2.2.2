5e01433c3d7bd033a70413b21cb4e59b3c40653b
ieroslav1 <yaraboom@rambler.ru>
2023-03-14 10:40:05 +0300

    Выполненное задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-   }
+
+        Bicycle stels = new Bicycle("Стелс", 2);
+        Bicycle kama = new Bicycle("Кама", 2);
+
+
+        Car lada = new Car("Лада", 4);
+        Car kia = new Car("Киа", 4);
+
+
+        Truck man = new Truck("Ман", 6);
+        Truck kamaz = new Truck("Камаз", 8);
+
+
+        ServiceStation station = new ServiceStation();
+        station.check(stels);
+        System.out.println();
+        station.check(kama);
+        System.out.println();
+        station.check(lada);
+        System.out.println();
+        station.check(kia);
+        System.out.println();
+        station.check(man);
+        System.out.println();
+        station.check(kamaz);
+    }
 }
